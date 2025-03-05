d61f10812e8af2dad1b98bef086c742dce3e540c
quinaty <cmwl22334455@163.com>
2025-03-05 15:01:28 +0800

    chore: add main function 添加主函数

--- main.py
+++ main.py
@@ -18,3 +18,16 @@ def document_similarity(file_processor):
     answer = '二者的重复度为:' +(str( jaccard_similarity(org_set,add_set).__round__(2)))+'\n'
     fp.file_write(file_processor.file_paths[2], str(answer))
 
+def main():
+    org_file, org_add_file, answer_file = fi.read_file_from_args()
+    file_processor = fp.FileProcessor([org_file, org_add_file, answer_file])
+
+    fp.file_write(file_processor.file_paths[2], '原文件：'+file_processor.file_paths[0]+'\n')
+    fp.file_write(file_processor.file_paths[2], '检查文件：'+file_processor.file_paths[1]+'\n')
+
+    document_similarity(file_processor)
+    print(fp.file_read(file_processor.file_paths[2]),end='\n\n')
+
+
+if __name__ == '__main__':
+    main()
