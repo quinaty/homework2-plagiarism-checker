519d7658c93d9311dd7ab7e1cc2ca84e39989ba6
quinaty <cmwl22334455@163.com>
2025-03-05 12:44:40 +0800

    chore: Add new features 添加新功能

--- file_processing.py
+++ file_processing.py
@@ -20,6 +20,28 @@ def file_read(file_path):
 
     return None
 
+# 写入文件内容
+def file_write(file_path, data):
+
+    try:
+        with open(file_path, 'a+', encoding='utf-8') as file:
+                file.write(data)
+    except FileNotFoundError:
+        print(f"错误：文件 '{file_path}' 不存在")
+    except IsADirectoryError:
+        print(f"错误：'{file_path}' 是一个目录")
+    except PermissionError:
+        print(f"错误：没有权限写入 '{file_path}'")
+    except UnicodeDecodeError:
+        print("错误：文件编码不支持，请尝试指定其他编码方式")
+    except Exception as e:
+        print(f"写入文件时发生未知错误：{str(e)}")
+
+
+
+
+
+
 class FileProcessor:
     def __init__(self, file_paths):
         self.file_paths = file_paths
